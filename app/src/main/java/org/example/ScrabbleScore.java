e0db4e0cbc79be2222a4bb12c5712683047da227
Barry <barryrichards05@gmail.com>
2026-02-27 16:10:36 +0000

    implemented ScrabbleScore with calculate method

---
+++ app/src/main/java/org/example/ScrabbleScore.java
@@ -0,0 +1,46 @@
+package org.example;
+
+import java.util.HashMap;
+
+public class ScrabbleScore {
+
+    private static HashMap<Character, Integer> letterScores = new HashMap<>();
+
+    static {
+        letterScores.put('a', 1);
+        letterScores.put('b', 3);
+        letterScores.put('c', 3);
+        letterScores.put('d', 2);
+        letterScores.put('e', 1);
+        letterScores.put('f', 4);
+        letterScores.put('g', 2);
+        letterScores.put('h', 4);
+        letterScores.put('i', 1);
+        letterScores.put('j', 8);
+        letterScores.put('k', 5);
+        letterScores.put('l', 1);
+        letterScores.put('m', 3);
+        letterScores.put('n', 1);
+        letterScores.put('o', 1);
+        letterScores.put('p', 3);
+        letterScores.put('q', 10);
+        letterScores.put('r', 1);
+        letterScores.put('s', 1);
+        letterScores.put('t', 1);
+        letterScores.put('u', 1);
+        letterScores.put('v', 4);
+        letterScores.put('w', 4);
+        letterScores.put('x', 8);
+        letterScores.put('y', 4);
+        letterScores.put('z', 10);
+    }
+
+    public static int calculate(String word){
+        int total = 0;
+        for (int i = 0; i < word.length(); i++) {
+            Character letter = word.charAt(i);
+            total += letterScores.get(letter);
+        }
+        return total;
+    }
+}
---
+++ app/src/test/java/org/example/TestScrabbleScore.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestScrabbleScore {
+
+    @Test
+    public void simpleScores() {
+        Assert.assertEquals(2, ScrabbleScore.calculate("at"));
+
+        Assert.assertEquals(3, ScrabbleScore.calculate("not"));
+
+        Assert.assertEquals(4, ScrabbleScore.calculate("neat"));
+    }
+
+    @Test
+    public void complexScores() {
+        Assert.assertEquals(5, ScrabbleScore.calculate("gate"));
+
+        Assert.assertEquals(29, ScrabbleScore.calculate("jazz"));
+
+        Assert.assertEquals(21, ScrabbleScore.calculate("quintuplet"));
+    }
+}
