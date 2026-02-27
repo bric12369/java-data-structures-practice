be658eeac56bc9e9cd4946485d4361579407faaa
Barry <barryrichards05@gmail.com>
2026-02-27 15:10:42 +0000

    implemented queue class with add and state methods

--- app/src/main/java/org/example/Queue.java
+++ app/src/main/java/org/example/Queue.java
@@ -1,4 +1,22 @@
 package org.example;
 
+import java.util.ArrayList;
+
 public class Queue {
+
+    ArrayList<String> peopleInQueue = new ArrayList<>();
+
+    public static void main(String[] args) {
+        Queue queue = new Queue();
+        queue.add("Simon");
+        System.out.println(queue.state());
+    }
+
+    public void add(String name){
+        peopleInQueue.add(name);
+    }
+
+    public ArrayList<String> state(){
+        return this.peopleInQueue;
+    }
 }
--- app/src/test/java/org/example/TestQueue.java
+++ app/src/test/java/org/example/TestQueue.java
@@ -1,4 +1,22 @@
 package org.example;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
 public class TestQueue {
+    @Test
+    public void canAddAndStatePeople() {
+        Queue queue = new Queue();
+        queue.add("Simon");
+        ArrayList<String> result = queue.state();
+        Assert.assertEquals("Simon", result.get(0));
+
+        queue.add("Jack");
+        ArrayList<String> result2 = queue.state();
+        Assert.assertEquals(List.of("Simon", "Jack"), result2);
+    }
 }
